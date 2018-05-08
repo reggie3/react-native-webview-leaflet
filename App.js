c0eb09563b93e9c0ec143b94ef2ea5bf98f216fb
Reginald Johnson <reginald.johnson@gmail.com>
2018-05-08 15:54:20 -0400

    "publishing"

--- App.js
+++ App.js
@@ -220,6 +220,7 @@ export default class App extends React.Component {
           onMove={this.onMove}
           onZoomEnd={this.onZoomEnd}
           onMoveEnd={this.onMoveEnd}
+          showMapAttribution ={false}
         />
         <View
           style={{
