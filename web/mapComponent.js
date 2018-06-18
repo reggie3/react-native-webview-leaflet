4524ea321ea5d1827c2b4538e443532df0fd0a9a
Reginald Johnson <reginald.johnson@gmail.com>
2018-06-18 13:25:30 -0400

    "publishing"

--- web/mapComponent.js
+++ web/mapComponent.js
@@ -4,12 +4,13 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import L, { divIcon } from 'leaflet';
 import testLocations from './testLocations';
 import './markerAnimations.css';
-import './leaflet.css';
 import '/normalize.min.css';
 
+import '.leaflet/dist/leaflet.css';
 import 'leaflet/dist/images/marker-icon-2x.png';
 import 'leaflet/dist/images/marker-shadow.png';
 import './markers.css';
+
 const isValidCoordinates = require('is-valid-coordinates');
 const util = require('util');
 
